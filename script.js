1c2ce6326f464d6df8de7d9c6a71306c7be6f6d9
Thatchapon Chatthanes <chatthanes22@gmail.com>
2023-10-13 16:56:02 +0700

    Add Lecturers 2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let header = document.querySelector(".navigation");
 
+// Navigation Bar Scroll Fixed
 window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.scrollY);
 });
